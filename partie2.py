01c60b0921ae085bf6fd0e4dc0348aced993a257
ptribel <ptribel@ulb.ac.be>
2018-11-04 18:48:28 +0100

    Commentaires

--- partie2.py
+++ partie2.py
@@ -2,19 +2,34 @@
 
 class Node:
 	def __init__(self, value, hyperAretes=[]):
+		"""
+		Value : valeur du noeud
+		hyperAretes : hyper-arêtes qui contiennent le noeud
+		"""
 		self.value = value
 		self.hyperAretes = hyperAretes
+		
 	def __str__(self):
 		return str(self.value)
+		
 	def getValue(self):
 		return self.value
+		
 	def setValue(self, v):
 		self.value = v
+		
 	def appendHyperArete(self, h):
+		"""
+		Permet d'annoncer que le noeud appartient à l'hyper-arête h
+		"""
 		self.hyperAretes.append(h)	
 	
 class HyperArete:
 	def __init__(self, *nodes):
+		"""
+		nodes : les noeuds qui sont contenus dans l'hyper-arête
+		Ceux-ci sont modifiés pour indiquer qu'ils sot-nt contenus dans l'hyper-arête
+		"""
 		self.nodes = nodes
 		for i in self.nodes:
 			i.appendHyperArete(self)
